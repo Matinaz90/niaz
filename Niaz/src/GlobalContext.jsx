388c23d0f502b942c848a80a72947f824bfe64a4
Matinaz90 <azizmohammadimatin828@gmail.com>
2025-12-01 14:12:11 +0330

    starting

--- Niaz/src/GlobalContext.jsx
+++ Niaz/src/GlobalContext.jsx
@@ -10,12 +10,23 @@ export function GlobalProvider({ children }) {
       const bar = document.getElementById("rightBar");
       if (bar && !bar.contains(e.target)) {
         setOpenRightBar(false);
-        document.getElementById('blur_rightBarOpen').style.display = 'none';
+
       };
     };
     document.addEventListener("mousedown", handleClick);
     return () => document.removeEventListener("mousedown", handleClick);
   }, []);
+
+
+  useEffect(() => {
+  const exitBtn = document.getElementById("exitBtn");
+  const blur = document.getElementById("blur_rightBarOpen");
+
+  if (!exitBtn || !blur) return;
+
+  exitBtn.classList[openRightBar ? "add" : "remove"]("open");
+  blur.classList[openRightBar ? "add" : "remove"]("open");
+}, [openRightBar]);
 
   useEffect(() => {
   document.body.style.overflow = openRightBar ? 'hidden' : 'auto';
